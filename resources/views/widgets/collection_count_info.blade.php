c2d6bf0ae40bb63ccd962a860bf3857bd3d428fe
Philipp Daun <post@philippdaun.net>
2026-02-12 16:03:59 +0100

    Use accent color on hover
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- resources/views/widgets/collection_count_info.blade.php
+++ resources/views/widgets/collection_count_info.blade.php
@@ -1,15 +1,15 @@
 @php use function Statamic\trans as __; @endphp
 
-<div class="group -mt-1.5! -mb-1! text-5xl hover:opacity-70">
-    <p class="text-base truncate flex gap-2 items-baseline">
+<div class="group -mt-1.5! -mb-1! text-5xl">
+    <p class="text-base truncate flex gap-2 items-baseline group-hover:text-ui-accent-text">
         <span>{{ __($collection->title) }}</span>
         @if ($collection->url)
-            <span class="opacity-0 group-hover:opacity-100">→</span>
+            <span class="opacity-0 group-hover:opacity-100 group-hover:text-ui-accent-text">→</span>
         @endif
     </p>
     <div class="mt-1! -ml-0.5! lining-nums font-medium">
         <p>
-            <span>{{ $collection->count }}</span>
+            <span class="group-hover:text-ui-accent-text">{{ $collection->count }}</span>
         </p>
     </div>
     @if ($collection->url)
