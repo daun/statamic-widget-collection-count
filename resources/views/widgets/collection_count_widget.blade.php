0100f43e2051f3d80cb32b7e533da347fd32d6a8
Philipp Daun <post@philippdaun.net>
2026-02-12 19:37:56 +0100

    Tweak cardless layout
    Signed-off-by: Philipp Daun <post@philippdaun.net>

--- resources/views/widgets/collection_count_widget.blade.php
+++ resources/views/widgets/collection_count_widget.blade.php
@@ -7,7 +7,7 @@
 @if ($grid && $cards)
     <div class="@container grid grid-cols-[repeat(auto-fill,minmax(clamp(4em,100%/9,6em),1fr))] gap-x-6 gap-y-4 text-5xl">
 @elseif ($grid)
-    <div class="@container grid grid-cols-[repeat(auto-fill,minmax(clamp(3.5em,100%/10,5.5em),1fr))] gap-4 pt-2! pb-3! text-5xl">
+    <div class="@container grid grid-cols-[repeat(auto-fill,minmax(clamp(3.25em,100%/10,5.5em),1fr))] gap-x-6 gap-y-4 [&>div:not(:last-child)]:border-r! [&>div:not(:last-child)]:border-gray-200! [&>div:not(:last-child)]:dark:border-gray-700! pt-2! pb-3! text-5xl">
 @endif
 
     @foreach ($collections as $collection)
